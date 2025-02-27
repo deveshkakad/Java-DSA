673797e14e31998ddc29a6431e700caa6e899af8
deveshkakad <deveshkakad2016@gmail.com>
2025-02-27 20:37:30 +0530

    Bubble sort code

---
+++ sorting/BubbleSort.java
@@ -0,0 +1,26 @@
+public class BubbleSort{
+    public static void main(String[] args) {
+        int arr[] = {2,1,45,32,5,212};
+        bubbleSort(arr);
+
+        printArr(arr);
+    }
+
+    static void bubbleSort(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+    static void printArr(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+}
