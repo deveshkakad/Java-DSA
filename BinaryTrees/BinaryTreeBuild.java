e39db362e868c9f155eedd3dcd8d27f980e6d4bd
deveshkakad <deveshkakad2016@gmail.com>
2025-03-30 14:10:34 +0530

    Binary Tree Build from preorder

---
+++ BinaryTrees/BinaryTreeBuild.java
@@ -0,0 +1,45 @@
+public class BinaryTreeBuild{
+    public static void main(String[] args) {
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        Node root = BinaryTree.buildBinaryTreeFromPreOrder(nodes);
+        printBinaryTree(root);
+        System.out.println();
+    }
+
+    static void printBinaryTree(Node root){
+        if(root == null){
+            return;
+        }
+
+        System.out.print(root.data+" ");
+        printBinaryTree(root.left);
+        printBinaryTree(root.right);
+    }
+}
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinaryTree{
+    static int idx = -1;
+    static Node buildBinaryTreeFromPreOrder(int nodes[]){
+        idx++;
+
+        if(nodes[idx] == -1){
+            return null;
+        }
+
+        Node newNode = new Node(nodes[idx]);
+        newNode.left = buildBinaryTreeFromPreOrder(nodes);
+        newNode.right = buildBinaryTreeFromPreOrder(nodes);
+        return newNode;
+    }
+}
